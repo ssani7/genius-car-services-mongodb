37f9080e70f80e138723ec4cc194bb339167d4b4
ssani7 <sanaullah.sani756@gmail.com>
2022-05-07 12:54:44 -0600

    first commit

---
+++ __server_deploy.js
@@ -0,0 +1,3 @@
+/* 
+1. heroku login in cmd
+*/
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+const verifyJWT = (req, res, next) => {
+    // getting token from client
+    const authHeader = req.headers.auhtorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aednt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -20,7 +35,18 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
+        const orderCollection = client.db('geniusCar').collection('order');
 
+        // jwt
+        app.post('/login', (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // get all services from database
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -28,12 +54,14 @@ async function run() {
             res.send(result);
         });
 
+        // insert service in database
         app.post('/service', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result);
         })
 
+        // get specific service from database
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -41,10 +69,34 @@ async function run() {
             res.send(result);
         })
 
+        // remove specific service from db
         app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // for order collectin
+        // get all orders
+        app.get('/orders/', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (decodedEmail === email) {
+                const cursor = orderCollection.find({ email: email })
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
+        })
+
+        // insert order in db
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
